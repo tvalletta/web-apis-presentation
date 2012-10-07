83c577222780fc01c174da83f5dddd238d20edc5
Gabriel Dayley <gabriel.dayley@gmail.com>
2012-10-07 15:27:34 -0600

    getUserMedia demo

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express');
-var app = express.createServer();
+var app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
-app.configure(function(){
-    app.use(express.bodyParser());
-});
+server.listen(process.env.VCAP_APP_PORT || 3000);
 
-app.listen(process.env.VCAP_APP_PORT || 3000);
+
+//app.configure(function(){
+//    app.use(express.bodyParser());
+//});
+
 
 app.use("/", express.static(__dirname + '/static'));
+
+
+io.sockets.on('connection', function (socket) {
+    socket.on('message', function(msg){
+        console.log('message', msg);
+        socket.broadcast.emit('msg', msg);
+    });
+});
